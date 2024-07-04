56f0fd68e2ba590b3f420942c39e86939f4555ab
Orkar504+ <jose7211963@gmail.com>
2024-07-04 17:54:31 -0600

    agregado las rutas para los botones

--- app/Http/Controllers/directorioController.php
+++ app/Http/Controllers/directorioController.php
@@ -6,5 +6,36 @@ use Illuminate\Http\Request;
 
 class directorioController extends Controller
 {
-    //
+    public function verDirectorio()
+    {
+        return view ('directorio');
+    }
+
+    public function verEntrada()
+    {
+        return view ('crearEntrada');
+    }
+
+    public function verBuscar()
+    {
+        return view ('buscar');
+    }
+
+    public function verContactos()
+    {
+        return view ('vercontactos');
+    }
+
+    public function verEliminar()
+    {
+        return view ('eliminar');
+    }
+
+    public function destruir()
+    {
+        //destruir el registro
+    }
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\contactoController;
+use App\Http\Controllers\directorioController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/directorio', [directorioController::class,'verDirectorio']);
+
+//Botones
+Route::get('/directorio/entrada', [directorioController::class,'verEntrada'])
+->name('dir.ver.nuevo');
+
+Route::get('/directorio/eliminar', [directorioController::class,'verEliminar'])
+->name('dir.ver.eliminar');
+
+Route::get('/directorio/eliminar/no', [directorioController::class,'verDirectorio'])
+->name('dir.ver.eliminar.no');
+
+Route::get('/directorio/eliminar/si', [directorioController::class,'verDirectorio'])
+->name('dir.ver.eliminar.si');
+
+Route::get('/directorio/contactos', [directorioController::class,'vercontactos'])
+->name('dir.ver.contactos');
+
+Route::get('/directorio/buscar', [directorioController::class,'verBuscar'])
+->name('dir.ver.buscar');
+
