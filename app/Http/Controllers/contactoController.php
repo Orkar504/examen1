959cfc74a235d3e657eada015f04b8877c4247b9
Orkar504+ <jose7211963@gmail.com>
2024-07-04 18:15:38 -0600

    agregado funciones vacias

--- app/Http/Controllers/contactoController.php
+++ app/Http/Controllers/contactoController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class contactoController extends Controller
 {
-    //
+    public function crearContacto()
+    {
+
+    }
+
+    public function getContacto()
+    {
+
+    }
+
+    public function editContacto()
+    {
+
+    }
+
+    public function destroyContacto()
+    {
+        
+    }
+
 }
